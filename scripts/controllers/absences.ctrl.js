a562889685047d5f7fcd7911e5de8ce96c5d1efa
Fred <frederic72.gautier72@gmail.com>
2016-01-21 09:56:14 +0100

    construction d'un css card.css pour les blocks (on mettra surement le css des tuiles in)

--- scripts/controllers/absences.ctrl.js
+++ scripts/controllers/absences.ctrl.js
@@ -4,123 +4,89 @@
 angular
     .module('portail.controllers')
     .controller('AbsencesCtrl',
-   function($scope, $compile, $timeout, uiCalendarConfig) {
+   function($scope, $compile, $modal, $timeout, uiCalendarConfig) {
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
 
-
-     $scope.eventSource = {
-             url: "http://calendar.google.com/calendar/ical/fr.french%23holiday%40group.v.calendar.google.com/public/basic.ics",
-             className: 'gcal-event',           // an option!
-             currentTimezone: 'local' // an option!
-     };
-
-    /* remove event */
-    $scope.remove = function(index) {
-      $scope.events.splice(index,1);
+    /* event source that contains custom events on the scope */
+    $scope.events = [];
+    $scope.eventSources = [];
+    /* event source that calls a function on every view switch */
+    $scope.eventsF = function (start, end, timezone, callback) {
+      var s = new Date(start).getTime() / 1000;
+      var e = new Date(end).getTime() / 1000;
+      var m = new Date(start).getMonth();
+      var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
+      callback(events);
     };
-    /* Change View */
-
-    $scope.renderCalender = function(calendar) {
-      $timeout(function() {
-        if(uiCalendarConfig.calendars[calendar]){
-          uiCalendarConfig.calendars[calendar].fullCalendar('render');
-        }
-      });
-    };
-     /* Render Tooltip */
-    $scope.eventRender = function( event, element, view ) {
-        element.attr({'tooltip': event.title,
-                      'tooltip-append-to-body': true});
-        $compile(element)($scope);
-    };
-
-
+    
     /* config object */
     $scope.uiConfig = {
-      calendar:{
-        unselectAuto: false,
-        selectable: true,
-        selectHelper: true,
-        weekNumbers: true,
+        calendar:{
+        height: 550,
         editable: true,
+        selectable: true,
+        weekends: true,
         header:{
-          left: 'prev',
-          center: 'title',
-          right: 'next'
+          left: 'title',
+          center: '',
+          right: 'today prev,next'
         },
-        select: function(start, end, allDay) {
-                start=moment(start).format('YYYY-MM-DD');
-                end=moment(end).format('YYYY-MM-DD');
-                var mywhen = start + ' au ' + end;
-
-
-
-             },
-        eventClick: $scope.alertOnEventClick,
+       /* select: function(start, end, event) {
+                console.log(event);
+                if(start == end){
+                  $scope.alertOnEventClick;  
+                }
+                var dialogOpts = {
+                    backdrop: true,
+                    keyboard: true,
+                    templateUrl: './views/modaleTpl.html', // Url du template HTML
+                    controller: ['$scope', '$modalInstance','scopeParent', 'id',
+                        function($scope, $modalInstance,scopeParent,id) { //Controller de la fenêtre. Il doit prend en paramètre tous les élèments du "resolve".
+                            $scope.delete = function() {
+                                //On fait appel à la fonction du scope parent qui permet de supprimer l'élément.
+                                //On peut également faire appel à un service de notre application.
+                                scopeParent.delete(id);
+                                //Fermeture de la fenêtre modale
+                                $modalInstance.close();
+                            };
+                            $scope.cancel = function() {
+                                // Appel à la fonction d'annulation.
+                                $modalInstance.dismiss('cancel');
+                            };
+                        }
+                    ],
+                    resolve: {
+                        scopeParent: function() {
+                            return $scope; //On passe à la fenêtre modal une référence vers le scope parent.
+                        },
+                        id: function(){
+                            return $scope.id; // On passe en paramètre l'id de l'élément à supprimer.
+                        }
+                    }
+                };
+                $modal.open(dialogOpts);
+        },*/
+        events: [
+            {
+                title: 'All Day Event',
+                start: '2016-01-01'
+            },
+            {
+                title: 'Long Event',
+                start: '2016-01-07',
+                end: '2016-01-10'
+        }],
+        eventClick: function(){alert("tete")},
         eventDrop: $scope.alertOnDrop,
         eventResize: $scope.alertOnResize,
-        eventRender: $scope.eventRender
+        eventRender: $scope.eventRender,
+        dayClick: function(){alert("tete")},
       }
     };
-
-    $scope.uiConfig.calendar.dayNames = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
-    $scope.uiConfig.calendar.dayNamesShort = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"];
-
-
     /* event sources array*/
     $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
     $scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
-
-   /* $scope.submitButton.on('click', function(e){
-    	e.preventDefault();
-		  doSubmit();
-    });*/
-
-    function doSubmit(){
-	    function diffdate(d1,d2,u){
-			     var div=1
-			        switch(u){
-		 		        case 's': div=1000;
-		 			 		  break;
-			 	        case 'm': div=1000*60
-						 	  break;
-			 	        case 'h': div=1000*60*60
-							  break;
-				        case 'd': div=1000*60*60*24
-							  break;
-			        }
-
-		       var Diff = d2.getTime() - d1.getTime();
-		       return Math.ceil((Diff/div))
-		}
-
-    // -------------------- journée complète : pas de fréquence -----------------------------  //
-
-
-    if(($scope.conge.is(':checked')) && ($scope.journee.is(':checked')) && ($scope.pas_frequence.is(':checked'))){
-        var titre = "Congés";
-        var color = "rgba(1,66,110,0.5)";
-
-        alert(nb_conge-nb_days+" pas de fréquence");
-    }
-
-    else if (($scope.rttQ1.is(':checked')) && ($scope.journee.is(':checked')) && ($scope.pas_frequence.is(':checked'))){
-    	var titre = "RTT Q1";
-    	var color = "rgba(14,130,188,0.5)";
-
-    	alert(nb_rttQ1-nb_days+" pas de fréquence");
-    }
-    else if (($scope.rttQ2.is(':checked')) && ($scope.journee.is(':checked')) && ($scope.pas_frequence.is(':checked'))){
-    	var titre = "RTT Q2";
-    	var color = "rgba(52,204,227,0.5)";
-
-    	alert(nb_rttQ2-nb_days+" pas de fréquence");
-    }
-    else if (($scope.autre_conge.is(':checked')) && ($scope.journee.is(':checked')) && ($scope.pas_frequence.is(':checked'))){
-    	var titre = "Autres";
-    	var color = "rgba(121,158,169,0.54)";
-
-    	alert(nb_autre-nb_days+" pas de fréquence");
-    };
-};
-
 });
